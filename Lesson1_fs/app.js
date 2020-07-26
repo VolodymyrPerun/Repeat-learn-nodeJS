fd2a2d8a94dba31df37f2a1010230380ac2571e6
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-07-27 02:57:58 +0300

    innit

--- Lesson1_fs/app.js
+++ Lesson1_fs/app.js
@@ -9,9 +9,37 @@ const path = require('path');
 const fs = require('fs');
 
 const os = require('os');
-console.log(os.platform());
 
-fs.writeFile(path.join(__dirname, 'files', 'data.txt'), 'Hello, World! \n', err =>{
+// console.log(os.platform());
+//
+// fs.writeFile(path.join(__dirname, 'files', 'data.txt'), 'Hello, World! \n', err => {
+//     console.log(err);
+// })
+//
+//
+// for (let i = 0; i < 10; i++) {
+//     fs.appendFile(path.join(__dirname, 'files', 'data.txt'), 'Hello, World! \n', {flag: 'a'}, err => {
+//         console.log(err);
+//     })
+// }
+//
+//
+//
+//     fs.readFile(path.join(__dirname,'files', '40543828_287549665180409_5606730224654876672_n.mp4'), (err, data) => {
+//         console.log(data.toString());
+//     })
+
+
+
+// fs.stat(path.join(__dirname, 'files', 'data.txt'), (err, stats) => {
+//     console.log(stats);
+// })
+
+fs.mkdir(path.join(__dirname, 'папка'), err => {
     console.log(err);
 })
 
+
+fs.rmdir(path.join(__dirname, 'папка'), err => {
+    console.log(err);
+})
