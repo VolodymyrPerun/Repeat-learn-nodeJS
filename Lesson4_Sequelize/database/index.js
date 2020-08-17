ec512e0bd582446f7f75a896c69c85424569ece5
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-08-17 21:51:38 +0300

    how to use, connekt sequelize

--- Lesson4_Sequelize/database/index.js
+++ Lesson4_Sequelize/database/index.js
@@ -1,4 +1,4 @@
-const Sequalize = require("sequelize")
+const Sequelize = require("sequelize")
 const fs = require("fs")
 const path = require("path")
 
@@ -6,19 +6,25 @@ module.exports = (() => {
     let instance;
 
     function innitConnection() {
-        const client = new Sequalize('shop', 'root', 'root', {
+        const client = new Sequelize('shop', 'root', 'root', {
             host: 'localhost',
             dialect: 'mysql'
         })
 
         let models = {}
 
-        function getModel() {
+        function getModels() {
             fs.readdir(path.join(process.cwd(), 'database', 'models'), (err, files) => {
                 files.forEach(file => {
                     const [modelName] = file.split('.')
+                    models[modelName] = client.import(path.join(process.cwd(), 'database', 'models', modelName))
                 })
             })
+        }
+
+        return {
+            setModels: () => getModels(),
+            getModel: (modelName) => models[modelName]
         }
     }
 
