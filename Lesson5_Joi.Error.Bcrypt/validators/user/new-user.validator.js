32a8e635a4150edcfe29b4a0abfb4f837ae6fa38
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-08-20 16:47:29 +0300

    Update new-user.validator.js

--- Lesson5_Joi.Error.Bcrypt/validators/user/new-user.validator.js
+++ Lesson5_Joi.Error.Bcrypt/validators/user/new-user.validator.js
@@ -8,10 +8,10 @@ module.exports = Joi.object().keys({
     email: Joi.string().regex(regexpEmailEnum.EMAIL).required(),
     password: Joi.string().regex(regexpPasswordEnum.PASSWORD).trim().min(8).required(),
     age: Joi.number().integer().min(1).max(120).required(),
-    description: Joi.string().optional().allow(null, ''),
-    cars: Joi.array().items(
-        Joi.object().keys({
-            name: Joi.string()
-        })
-    ).optional()
+    // description: Joi.string().optional().allow(null, ''),
+    // cars: Joi.array().items(
+    //     Joi.object().keys({
+    //         name: Joi.string()
+    //     })
+    // ).optional()
 })
